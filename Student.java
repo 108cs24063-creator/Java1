0c59490590d961c7ab108bf379826d8a851b489c
Yash <108CS24063@gmail.comgit config --global user.email 108CS24063@gmail.com>
2026-08-13 13:14:12 +0530

    Added Java Program

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    public static void main(String[] args) {
+            String name = "Tejas";
+            int age = 20;
+            String course = "Computer Science";
+            double percentage = 85.5;
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Course: " + course);
+            System.out.println("Percentage: " + percentage);
+    }
+}
